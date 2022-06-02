d5dd495be60d950253f16f0ed1343a9d457d6b6c
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-02 19:51:12 +0100

    Cleaning up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Profile from "./components/Profile";
-import Social from "./components/Social";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Header />
-      <Profile />
-      <Social />
     </div>
   );
 }

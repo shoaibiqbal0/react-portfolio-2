9632f99f83ddb5710b794869b8855a1de08a32ca
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-02 16:10:43 +0100

    Added icons in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { FaSun, FaMoon } from "react-icons/fa";
+import { Flex, Heading, IconButton, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [darkMode, setDardMode] = useState(false);
+
+  const handleClick = () => {
+    setDardMode(!darkMode);
+  };
+
+  return (
+    <VStack p="5">
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+          Hello
+        </Heading>
+      </Flex>
+      <IconButton onClick={handleClick} isRound="true">
+        {darkMode ? <FaSun /> : <FaMoon />}
+      </IconButton>
+    </VStack>
+  );
 }
 
 export default App;

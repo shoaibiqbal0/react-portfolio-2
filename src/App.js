3f8c3328af5e9eb099dcf78711e3c6dbe0af6f63
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-03 23:03:07 +0100

    Added home page text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <About />
     </div>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,22 @@
+import { Flex, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <VStack>
+      <Text fontSize="3xl" minW="400px" pt="5rem">
+        Hi, I'm
+      </Text>
+      <Text
+        fontSize="7xl"
+        bgGradient="linear(to-r, blue.300, purple.300, pink.300)"
+        bgClip="text"
+        fontWeight="bold"
+      >
+        Shoaib Iqbal
+      </Text>
+    </VStack>
+  );
 };
 
 export default About;

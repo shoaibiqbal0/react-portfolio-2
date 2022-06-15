5b7ae7e8343c5bca13a7703af8947ba5e681044c
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-15 21:16:19 +0100

    Fixed responsive behaviour of the main text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,21 +3,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <VStack>
-      <Text fontSize="2xl" minW="400px" pt="5rem">
-        Hi, I'm
-      </Text>
-      <Text
-        fontSize="7xl"
-        bgGradient="linear(to-r, blue.300, purple.300, pink.300)"
-        bgClip="text"
-        fontWeight="bold"
-      >
-        Shoaib Iqbal
-      </Text>
-      <Text fontSize="sm" color="gray.500">
-        A fullstack developer who creates beautiful and scalable applications.
-      </Text>
+    <VStack h="70vh" w="100vw" justify="center">
+      <VStack maxW={["300px", "500px"]}>
+        <Text w="100%" fontSize={["1xl", "2xl"]}>
+          Hi, I'm
+        </Text>
+        <Text
+          w="100%"
+          fontSize={["5xl", "7xl"]}
+          bgGradient="linear(to-r, blue.300, purple.300, pink.300)"
+          bgClip="text"
+          fontWeight="bold"
+        >
+          Shoaib Iqbal
+        </Text>
+        <Text w="100%" fontSize="sm" color="gray.500">
+          A fullstack developer who creates beautiful and scalable applications.
+        </Text>
+      </VStack>
     </VStack>
   );
 };
